109d285b3fbe50b8643ef1903fee95ebe0d8dc0f
tikyydc <ervinaksizov@gmail.com>
2026-04-26 14:24:01 +0300

    config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8648719412:AAHFqdjIjM3odZL_eFAJ7WSatq_bD2vGg7k")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "830297830"))
-PHONE_NUMBER = os.getenv("PHONE_NUMBER", "+7 (999) 844-11-02")
+PHONE_NUMBER = os.getenv("PHONE_NUMBER", "+7 (978) 844-11-02")
 BARBERSHOP_NAME = os.getenv("BARBERSHOP_NAME", "Барбершоп Строгоновка")
