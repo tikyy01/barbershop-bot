f4746b5b53501aaa0fce7448f795d00767086707
tikyydc <ervinaksizov@gmail.com>
2026-04-26 14:33:55 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8648719412:AAHFqdjIjM3odZL_eFAJ7WSatq_bD2vGg7k")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "830297830"))
-PHONE_NUMBER = os.getenv("PHONE_NUMBER", "+7 (978) 844-11-02")
-BARBERSHOP_NAME = os.getenv("BARBERSHOP_NAME", "Барбершоп Строгоновка")
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")
+BARBERSHOP_NAME = os.getenv("BARBERSHOP_NAME")
